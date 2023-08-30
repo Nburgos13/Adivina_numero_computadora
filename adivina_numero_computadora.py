f26c64077d93483aba185275e29647ed65d819ef
“Nicolas Burgos” <burgosdanielnicolas@gmail.com>
2023-08-30 00:05:01 -0300

    ciclo while - respuesta usuario

--- adivina_numero_computadora.py
+++ adivina_numero_computadora.py
@@ -1,1 +1,31 @@
-print("Hola Mundo")
+import random
+
+# Adivina el numero por la computadora
+
+
+
+def adivina_el_numero_computadora (x):
+
+    print("=" * 50)
+    print("Bienvenido(a) al juego!")
+    print("=" * 50)
+    print(f"Seleciona un numero entre el 1 y {x} para que la computadora intente adivinarlo...")
+
+    limite_inferior = 1
+    limite_superior = x
+
+    respuesta = ""
+
+    while respuesta != "c":
+
+        # Generar una prediccion
+
+        if limite_inferior != limite_superior:
+            prediccion = random.randint(limite_inferior, limite_superior)
+        else:
+            prediccion = limite_inferior
+
+        # Obtener una respuesta del usuario
+
+        respuesta = input(f"Mi prediccion es {prediccion}. Si es muy alta, ingresa (A). "
+                          f"Si es muy baja, ingresa (B). Si es correcta, ingresa (C).").lower()
